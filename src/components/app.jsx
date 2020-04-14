c500a3f1c96f7f94e5f288c1b5c7f9412eaa8a6a
Danny Peck <dannytpeck@gmail.com>
2020-04-14 15:49:33 -0700

    Update content and filtering for new upload

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -44,17 +44,17 @@ function App() {
 
     let timer = 0;
 
-    // Upload to coaching clients
+    // Upload to app clients
     const filteredClients = clients.filter(client => {
-      return client.fields['Failed Upload'] === '1';
+      return client.fields['Has App'] === 'Yes';
     });
 
     // Set counter based on filteredClients
     $('#counter').html(`<p><span id="finishedUploads">0</span> / ${filteredClients.length}</p>`);
 
     filteredClients.map(client => {
-      // 2 seconds between ajax requests, because limeade is bad and returns 500 errors if we go too fast
-      timer += 2000;
+      // 5 seconds between ajax requests, because limeade is bad and returns 500 errors if we go too fast
+      timer += 5000;
       setTimeout(() => {
         uploadChallenge(client);
       }, timer);
@@ -65,17 +65,17 @@ function App() {
     // Open the modal
     $('#uploadModal').modal();
 
-    const startDate = '2020-04-01';
-    const endDate = '2020-04-30';
-    const imageUrl = 'https://images.limeade.com/PDW/4db33758-07f3-434c-9045-da0b5d34c8b7-large.jpg';
-    const title = 'Hot Topic: Working From Home Wisely';
-    const activityText = 'listen to the recording';
-    const shortDescription = 'Complete the Hot Topics Podcast.';
+    const startDate = '2020-04-15';
+    const endDate = '2020-12-01';
 
-    const surveyId = 'f4da09d7-59ac-4536-a13b-a59c5d38245a';
+    const imageUrl = 'https://images.limeade.com/PDW/6fad1d13-2edd-45f2-9262-30d31bcccf13-large.jpg';
+    const title = 'COVID-19 Resources on the Aduro App';
+    const activityText = 'download the Aduro app';
+    const shortDescription = 'Download the Aduro App for timely resources and tools to help in the midst of uncertainty surrounding COVID-19.';
+    const longDescription = '<p>Download the Aduro app to access new learning sessions each week that provide tools and resources for all areas of life and well-being to help you navigate the changes and build resilience in the midst of uncertain times.</p><p>When we\'re up against change, uncertainty, and stress, resilience is the key to navigate life and emerge with more happiness and satisfaction. Each week, new learning sessions are delivered straight to your ADURO app so that you can access it wherever you are, right when you need it.</p><p>&nbsp;</p><p>Download the Aduro App via <a style="color: #4f81bd;" href="https://apps.apple.com/us/app/aduro/id1199288368?mt=8" target="_blank" rel="noopener">this link</a> for Apple products.</p><p>Download the Aduro App via <a style="color: #4f81bd;" href="https://play.google.com/store/apps/details?id=com.aduro.amp&amp;hl=en_US" target="_blank" rel="noopener">this link</a> for android users.</p>';
 
     const data = {
-      'AboutChallenge': '',
+      'AboutChallenge': longDescription,
       'ActivityReward': {
         'Type': 'IncentivePoints',
         'Value': '0'
@@ -88,7 +88,7 @@ function App() {
       'ChallengeType': 'OneTimeEvent',
       'Dimensions': [],
       'DisplayInProgram': startDate === moment().format('YYYY-MM-DD') ? true : false,  // sets true if the challenge starts today
-      'DisplayPriority': null,
+      'DisplayPriority': 100,
       'EndDate': endDate,
       'EventCode': '',
       'Frequency': 'None',
@@ -114,43 +114,19 @@ function App() {
       },
       contentType: 'application/json; charset=utf-8'
     }).done((result) => {
-      const surveyUrl = `/api/Redirect?url=https%3A%2F%2Fheartbeat.adurolife.com%2Fapp%2Fsurvey%2F%3Fs%3D${surveyId}%26q1%3D${result.Data.ChallengeId}%26q4%3D%5Bparticipantcode%5D%26q5%3D%5Be%5D`;
 
-      $.ajax({
-        url: 'https://api.limeade.com/api/admin/activity/' + result.Data.ChallengeId,
-        type: 'PUT',
-        dataType: 'json',
-        data: JSON.stringify({
-          'AboutChallenge': `<p>For many of us, the idea of working remotely was a new concept until recent events. If you are transitioning from working in an office to working at home, you're not alone. In this episode of Hot Topics, Coach Elyse explores some ways you can create some normalcy and structure while working from home.</p><p>Listen to the episode <a href="https://vimeo.com/adurolife/review/398697728/8791350656" target="_blank" rel="noopener">here.</a></p><p>After the podcast, be sure to fill out <a href="${surveyUrl}" target="_blank" rel="noopener">the survey</a>. We'd love to hear from you!</p><p style="font-size: 0.7em;">&copy; Copyright 2020 <a style="text-decoration: none;" href="http://www.adurolife.com/" target="_blank" rel="noopener">ADURO, INC.</a> All rights reserved.</p>`
-        }),
-        headers: {
-          Authorization: 'Bearer ' + client.fields['LimeadeAccessToken']
-        },
-        contentType: 'application/json; charset=utf-8'
-      }).done((result) => {
+      // Advance the counter
+      let count = Number($('#finishedUploads').html());
+      $('#finishedUploads').html(count + 1);
 
-        // Advance the counter
-        let count = Number($('#finishedUploads').html());
-        $('#finishedUploads').html(count + 1);
+      $('#uploadModal .modal-body').append(`
+        <div class="alert alert-success" role="alert">
+          <p>Uploaded Tile for <a href="${client.fields['Domain']}/ControlPanel/RoleAdmin/ViewChallenges.aspx?type=employer" target="_blank"><strong>${client.fields['Account Name']}</strong></a></p>
+          <p class="mb-0"><strong>Challenge Id</strong></p>
+        <p><a href="${client.fields['Domain']}/admin/program-designer/activities/activity/${result.Data.ChallengeId}" target="_blank">${result.Data.ChallengeId}</a></p>
+        </div>
+      `);
 
-        $('#uploadModal .modal-body').append(`
-          <div class="alert alert-success" role="alert">
-            <p>Uploaded Tile for <a href="${client.fields['Domain']}/ControlPanel/RoleAdmin/ViewChallenges.aspx?type=employer" target="_blank"><strong>${client.fields['Account Name']}</strong></a></p>
-            <p class="mb-0"><strong>Challenge Id</strong></p>
-          <p><a href="${client.fields['Domain']}/admin/program-designer/activities/activity/${result.Data.ChallengeId}" target="_blank">${result.Data.ChallengeId}</a></p>
-          </div>
-        `);
-
-      }).fail((request, status, error) => {
-        console.error(request.status);
-        console.error(request.responseText);
-        console.log('Update challenge failed for client', client.fields['Limeade e=']);
-      });
-
-    }).fail((request, status, error) => {
-      console.error(request.status);
-      console.error(request.responseText);
-      console.log('Create challenge failed for client ' + client.fields['Limeade e=']);
     });
 
   }
