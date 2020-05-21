2b06add179ee15bf5dab74e32dd9fe75b6847635
Josh Braun <braunjoshuaw@gmail.com>
2020-05-21 12:39:45 -0700

    Change subtitle again

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header id="header">
       <img src="images/logo.svg" />
       <h1 className="title">Eight Wolves</h1>
-      <h3>Conjure wolves to do your bidding</h3>
+      <h3>Conjure wolves to deploy challenges</h3>
     </header>
   );
 }
